4b68ae3748c5db5f13fdb4c3acf79dc2148c51c2
manitofighh <manitofigh@protonmail.com>
2023-04-09 12:01:32 -0400

    Function declaration

--- cardService.py
+++ cardService.py
@@ -1,34 +1,5 @@
 import random
 
-print("***********************************")
-print("***                             ***")
-print("***  Welcome to card services!  ***")
-print("***                             ***")
-print("***********************************")
-print()
-print("Choose a service: ")
-print("1) Credit Card Validator")
-print("2) Credit Card Generator")
-choice = input()
-choice.lower().replace(" ", "")
-arr1 = ["1", "1)", "validator", "creditcardvalidator", "cardvalidator", "creditvalidator"]
-arr2 = ["2", "2)", "generator", "creditcardgenerator", "cardgenerator", "creditgenerator"]
-
-if choice in arr1:
-    print()
-    userCard = input("Please enter your credit card number: ")
-    userCard = userCard.replace(" ", "").replace("-", "")
-    while (not userCard.isdigit() or len(userCard) != 16):
-      print()
-      userCard = input("Please enter your card number: ")
-      userCard = userCard.replace(" ", "").replace("-", "")
-
-
-
-
-
-# 2121 2121 2121 2121
-
 def checkCard(cardNum):
   sumOdd, sumEven = 0, 0
   for i in range (1, 16, 2):
@@ -60,11 +31,36 @@ def generateCard():
     randCard = random.randint(1000000000000000, 9999999999999999)
   return randCard
 
-if (checkCard(userCard)):
-  print()
-  print("Valid card number")
+print("***********************************")
+print("***                             ***")
+print("***  Welcome to card services!  ***")
+print("***                             ***")
+print("***********************************")
+print()
+print("Choose a service: ")
+print("1) Credit Card Validator")
+print("2) Credit Card Generator")
+choice = input()
+choice.lower().replace(" ", "")
+arr1 = ["1", "1)", "validator", "creditcardvalidator", "cardvalidator", "creditvalidator"]
+arr2 = ["2", "2)", "generator", "creditcardgenerator", "cardgenerator", "creditgenerator"]
+
+if choice in arr1:
+    print()
+    userCard = input("Please enter your credit card number: ")
+    userCard = userCard.replace(" ", "").replace("-", "")
+    while (not userCard.isdigit() or len(userCard) != 16):
+      print()
+      userCard = input("Please enter your card number: ")
+      userCard = userCard.replace(" ", "").replace("-", "")
+    if (checkCard(userCard)):
+      print()
+      print("Valid card number")
+    else:
+      print()
+      print("Invalid card number")
 else:
-  print()
-  print("Invalid card number")
+    print("Your input was not validated!")
+    exit()
 
 print("Generated card: " + str(generateCard()))
